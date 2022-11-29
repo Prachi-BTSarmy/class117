8512efe39dac67f276e64bd975b490f42795e689
prachi #BTS #VMIN <93324468+Prachi-BTSarmy@users.noreply.github.com>
2022-11-29 18:47:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 quick_draw_data_Set = ["lollipop" , "apple" , "strawberry" , "helicopter" , "candy" , "car" , "dog" , "cat" , "alarm clock" , "asparagous" , "laptop" , "computer"];
 random_no = Math.floor((Math.random()*quick_draw_data_Set.length)+1);
 Element_of_array = quick_draw_data_Set[random_number];
-document.getElementById("sketch_to_be_drawn").innerHTML = "sketch_to_be_drawn :"+ Element_of_array;
+document.getElementById("sketch").innerHTML = "sketch_to_be_drawn :"+ Element_of_array;
 
 timer_counter=0;
 timer_check="";
